0223446d769e7282a2b51f128a9c80983f6ae975
edseldim <edselmanuel@hotmail.com>
2022-12-04 12:05:26 -0500

    added config file handling in main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import os, json, subprocess
 
-with open("./config.json","r") as f:
-    env_variables = json.load(f)
-    for key, value in env_variables.items():
+try:
+    with open("./config.json","r") as f:
+        env_variables = json.load(f)
+        for key, value in env_variables.items():
+            os.environ[key] = value
+
+except FileNotFoundError:
+    env_variables_dict = {
+            "django_key":"lorem-ipsum"
+        }
+    with open("./config.json","w") as f:
+        json.dump(env_variables_dict,f)
+
+    for key, value in env_variables_dict.items():
         os.environ[key] = value
 
 subprocess.Popen(["python","web_scrapping\scheduler_main.py"],shell=True)
