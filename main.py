b0c7d602770841660f9e281982db685c3ecccd51
edseldim <edselmanuel@hotmail.com>
2022-12-23 23:59:56 -0500

    hotfix: env variables passed to processes in main.py

--- main.py
+++ main.py
@@ -17,5 +17,5 @@ my_env["django_key"] = my_env.get("django_key") or "DJANGO-INSECURE-KEY"
 my_env["webdriver_location"] = my_env.get("webdriver_location") or "web_scrapping/chromedriver.exe"
 my_env["scrapper_exec_time"] = my_env.get("scrapper_exec_time") or "60"
 
-subprocess.Popen([my_env["python_executable"],Path("web_scrapping","scheduler_main.py")])
-subprocess.run([my_env["python_executable"],Path("django_wcapp","django_wcapp","manage.py"),"runserver",my_env["django_server_address_port"]])
+subprocess.Popen([my_env["python_executable"],Path("web_scrapping","scheduler_main.py")],env=my_env)
+subprocess.run([my_env["python_executable"],Path("django_wcapp","django_wcapp","manage.py"),"runserver",my_env["django_server_address_port"]],env=my_env)
